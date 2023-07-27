f197506dab0d2b705d673356403b6c89c223be9a
sakuexe <saku.karttunen@gmail.com>
2023-07-27 15:54:01 +0300

    updated null-ls to work until a replacement will come along

--- nvim/lua/sakuexe/plugins/lsp/null-ls.lua
+++ nvim/lua/sakuexe/plugins/lsp/null-ls.lua
@@ -3,37 +3,27 @@ if not setup then
 	return
 end
 
-local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
--- to setup format on save
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
 	sources = {
-		formatting.prettier,
-		formatting.stylua,
-		-- formatting.djlint,
-		formatting.autopep8,
-		diagnostics.eslint_d,
-		diagnostics.djlint,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.diagnostics.eslint,
+		-- null_ls.builtins.completion.spell,
 	},
-	-- configure format on save
-	on_attach = function(current_client, bufnr)
-		if current_client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						filter = function(client)
-							--  only use null-ls for formatting instead of lsp server
-							return client.name == "null-ls"
-						end,
-						bufnr = bufnr,
-					})
-				end,
-			})
-		end
+	on_attach = function(client, bufnr)
+		-- if not client.supports_method("textDocument/formatting") then
+		-- 	return
+		-- end
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+				vim.lsp.buf.format({ bufnr = bufnr })
+			end,
+		})
 	end,
 })
