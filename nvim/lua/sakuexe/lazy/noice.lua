754bc9e461ea4f3ed482efb813c1a930de43a60e
sakuexe <saku.karttunen@gmail.com>
2024-02-03 08:27:03 +0200

    placed notify config to noice.lua

--- nvim/lua/sakuexe/lazy/noice.lua
+++ nvim/lua/sakuexe/lazy/noice.lua
@@ -1,19 +1,31 @@
 return {
-  "folke/noice.nvim",
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
+  {
     "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000", -- makes it shut up
+        render = "compact",
+      })
+    end,
   },
-  event = "VeryLazy",
-  opts = {
-    lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
       },
     },
-  },
+  }
 }
