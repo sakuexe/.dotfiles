d67849f5f80ca50e294b6437fb3bcbe9e70a3d0e
sakuexe <saku.karttunen@gmail.com>
2024-10-25 17:25:12 +0300

    actually fixed it this time

--- zsh/hotkeys.zsh
+++ zsh/hotkeys.zsh
@@ -3,9 +3,9 @@ bindkey -e
 # include some of the niceties of vi mode
 # without actually using vi mode
 # see all the hotkeys with bindkey -L
-bindkey "[[1~" beginning-of-line # home button
-bindkey "[[2~" yank
-bindkey "[[3~" delete-char # delete button
-bindkey "[[4~" end-of-line # end button
-bindkey "[[5~" history-beginning-search-backward-end
-bindkey "[[6~" history-beginning-search-forward-end
+bindkey "^[[1~" beginning-of-line # home button
+bindkey "^[[2~" yank
+bindkey "^[[3~" delete-char # delete button
+bindkey "^[[4~" end-of-line # end button
+bindkey "^[[5~" history-beginning-search-backward-end
+bindkey "^[[6~" history-beginning-search-forward-end
