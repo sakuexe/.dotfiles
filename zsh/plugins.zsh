cd2352de835c751582719d0677b2e212cfa71586
sakuexe <saku.karttunen@gmail.com>
2024-10-25 15:03:44 +0300

    added an os check to plugin loading

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,8 +1,3 @@
-# Syntax highlighting
-source "$ZPLUGINDIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-# Autosuggestions
-source "$ZPLUGINDIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
-
 # Other single file plugins
 plugins=($ZPLUGINDIR/*.zsh)
 if [ ${#plugins[@]} -gt 0 ]; then
@@ -10,3 +5,17 @@ if [ ${#plugins[@]} -gt 0 ]; then
         source "$plugin"
     done
 fi
+
+OS = grep '^NAME' /etc/os-release | sed 's/NAME=//'
+
+# do not source plugins on nixos, because they are loaded
+# from nixpkgs
+if [ "$OS" = "NIXOS" ]; then
+  exit 0
+fi
+
+# Syntax highlighting
+source "$ZPLUGINDIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+# Autosuggestions
+source "$ZPLUGINDIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
