c8389896579ec644b3d1c3306be6c6f53fdadaaf
Saku Karttunen <saku.karttunen@gmail.com>
2025-01-16 12:37:14 +0200

    improved the fix for templ formatting

--- nvim/lua/sakuexe/after/filetypes.lua
+++ nvim/lua/sakuexe/after/filetypes.lua
@@ -10,12 +10,20 @@ vim.api.nvim_create_autocmd("FileType", {
   end
 })
 
+-- returns a function that can be used as a keymap function
+-- the lsp param is the name of the language server you want to filter out
+function generateFilteredFormat(lsp)
+  return function()
+    vim.lsp.buf.format {
+      filter = function(client) return client.name ~= lsp end
+    }
+  end
+end
+
 -- templ: don't use gopls formatting
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "templ",
   callback = function()
-    vim.lsp.buf.format {
-      filter = function(client) return client.name ~= "gopls" end
-    }
+    vim.keymap.set("n", "<C-F>", generateFilteredFormat("gopls"), nil)
   end
 })
