0c6cd196bd944c84a17beb770bc87566856be2ba
Saku Karttunen <saku.karttunen@gmail.com>
2025-01-27 09:57:01 +0200

    switched to using tmux script to open sessions instead of windows
    With windows the workflow just felt a bit cluttered.

--- zsh/scripts/tmux-sessionizer.sh
+++ zsh/scripts/tmux-sessionizer.sh
@@ -1,7 +1,7 @@
 # based on the work of ThePrimeagen and his Tmux Sessionizer
 # https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
 
-tmux-windowizer () {
+tmux-sessionizer () {
   # check if fzf is installed
   if ! command -v fzf &> /dev/null; then
     echo "fzf is not installed"
@@ -18,13 +18,29 @@ tmux-windowizer () {
   fi
 
   selected_dir=$(echo $all_dirs | fzf)
+  selected_name=$(basename $selected_dir)
+  tmux_running=$(pgrep tmux)
 
   # early exit if nothing is selected
   if [[ -z $selected_dir ]]; then
     return
   fi
 
-  tmux new-window -c $selected_dir -n $(basename $selected_dir)
+  # if tmux is not already running, create a new session and attach it
+  if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected_dir 
+    return
+  fi
+
+  # if the session with the selected name does NOT exist,
+  # create a new session with the name WITHOUT attaching it
+  if ! tmux-has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected_dir 
+  fi
+
+  # attach to the chosen session
+  tmux switch-client -t $selected_name
 }
 
-# bindkey -s "^f" "tmux-windowizer\n"
+# ctrl-f keybind
+bindkey -s "^f" "tmux-sessionizer\n"
