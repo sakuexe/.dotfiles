f9f7e8142eb2dd2e98921e95b1b844fdd870a60a
Saku Karttunen <saku.karttunen@gmail.com>
2025-02-28 09:14:43 +0200

    fixed omp not installing and other tweaks to install script

--- install.sh
+++ install.sh
@@ -17,25 +17,6 @@ if [ "$OS" == '"Arch Linux"' ]; then
     git unzip ripgrep gcc g++ make
 fi
 
-# neovim
-ln -s $PWD/nvim ~/.config/nvim
-
-# tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
-
-# oh-my-posh
-if command -v oh-my-posh &> /dev/null; then
-  echo "oh-my-posh not found, installing..."
-  mkdir -p ~/.local/bin
-  if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-    export PATH="$PATH:$HOME/.local/bin"
-  fi
-  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
-fi
-
-ln -s $PWD/omp ~/.config/omp
-
 # zsh 
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ./zsh/plugins/zsh-autosuggestions
@@ -43,7 +24,25 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ./zsh/plugins/zsh-aut
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./zsh/plugins/zsh-syntax-highlighting
 
 ln -s $PWD/zsh/.zshenv ~/.zshenv
-ln -s $PWD/zsh ~/.config/zsh
-chsh -s /bin/zsh
+source ~/.zshenv # get the env variables, so we know where to place the files
+mkdir -p $XDG_CONFIG_HOME
+ln -s $PWD/zsh $ZDOTDIR
+sudo chsh -s /bin/zsh
 
-zsh # start zsh
+# oh-my-posh
+if ! command -v oh-my-posh &> /dev/null; then
+  echo "oh-my-posh not found, installing..."
+  mkdir -p ~/.local/bin
+  if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    export PATH="$PATH:$HOME/.local/bin"
+  fi
+  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+fi
+ln -s $PWD/omp $XDG_CONFIG_HOME/omp
+
+# neovim
+ln -s $PWD/nvim $XDG_CONFIG_HOME/nvim
+
+# tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
