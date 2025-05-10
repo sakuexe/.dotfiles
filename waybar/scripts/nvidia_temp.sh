a46ce1bab4aa7ff1985de5e704f6f925cd0b58f2
Saku Karttunen <saku.karttunen@gmail.com>
2025-05-10 15:42:33 +0300

    moved waybar elements to their own little groups too
    and other QoL improvements to waybar

--- waybar/scripts/nvidia_temp.sh
+++ waybar/scripts/nvidia_temp.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env sh
 
 TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits | head -n 1)
-echo "{\"text\": \" ${TEMP}°C\", \"tooltip\": \"NVIDIA GPU Temp\"}"
+
+# make the icon responsive!
+ICON=""
+
+if (( $TEMP > 40 )); then
+  ICON=""
+fi
+if (( $TEMP > 50 )); then
+  ICON=""
+fi
+if (( $TEMP > 60 )); then
+  ICON=""
+fi
+
+echo "{\"text\": \"${ICON} ${TEMP}°C\", \"tooltip\": \"NVIDIA GPU Temp\"}"
